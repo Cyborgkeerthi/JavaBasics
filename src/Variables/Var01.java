71e7945ce43ff402ff3cbbd51b25ee2c2aaf9eb5
cyborgkeerthi <keerthi05.kp@gmail.com>
2024-09-17 22:12:26 +0530

    Variables

---
+++ src/Variables/Var01.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Var01 {
+    public static void main(String[] args){
+        // variable : it is a container that stores the value
+        //int = data type , a = variable , 10 = value
+
+       // int a ; // Declaration
+       // a = 10; // Assignment
+       // System.out.println(a);
+
+        //int a =100;
+        //System.out.println(a);
+
+        int a,b,c;
+        a=13;
+        b=23;
+        c=44;
+       // System.out.println(a+b+c);// concat //output=80
+        System.out.println(a+" "+b+" "+c); //output =13 23 44
+        System.out.println(a+"\n"+b+"\n"+c);
+    }
+}
