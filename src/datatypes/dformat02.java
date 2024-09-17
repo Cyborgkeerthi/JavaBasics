e4c7699f476730faae8d2cf197590ea2abba8f96
cyborgkeerthi <keerthi05.kp@gmail.com>
2024-09-17 23:23:54 +0530

    Variables

---
+++ src/datatypes/dformat02.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class dformat02 {
+    public static void main(String[] args){
+        int a=122;
+        byte b=100;
+        System.out.printf("%d  %d",a,b); // %d = Base 10 integer, such as a Java int, long, short or byte
+        System.out.printf("\n%d * %d = %d",a,2,244);
+
+        // %b = boolean true or false value
+        //%s = string
+
+        String city="cochi";
+        System.out.printf("\nmy city is %s",city);
+    }
+
+}
