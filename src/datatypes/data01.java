40f0f8cad9205e359d7803e665efbd3e29fd900c
cyborgkeerthi <keerthi05.kp@gmail.com>
2024-09-17 22:53:04 +0530

    Variables

---
+++ src/datatypes/data01.java
@@ -0,0 +1,43 @@
+package datatypes;
+
+public class data01 {
+    public static void main(String[] args){
+        // numeric data types
+        int a =19;
+        int c=23;
+        System.out.println("the number is :"+ a);// output = the number is :19
+        System.out.println("the number is :"+ a+c); //output = the number is :1923
+
+        byte b=20;
+        System.out.println(b);
+
+        short s=3344;
+        System.out.println(s);
+
+        long k=2636363638837777l;
+        System.out.println(k);
+
+        // decimal
+        float f=4.5f; // literal is need F/f
+        System.out.println(f);
+
+        double d= 45.444;
+        System.out.println(d);
+
+        char hh='d'; // single character data type , single quotation
+        System.out.println(hh);
+
+        String name ="karthik"; // multiple character data type , double quotation
+        System.out.println(name);
+
+        boolean p= true;
+        System.out.println(p);
+
+       // char='hdggd'; // invalid
+
+
+    }
+
+
+
+}
