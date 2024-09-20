004f07040d1e40ad5782a052f004d924a852d793
cyborgkeerthi <keerthi05.kp@gmail.com>
2024-09-20 15:18:27 +0530

    Sample

---
+++ src/datatypes/example.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+public class example {
+    public static void main (String[] args){
+        char a='A'; // Asci value
+        char b='F';// Ascci value
+        System.out.println(a+b);
+    }
+}
